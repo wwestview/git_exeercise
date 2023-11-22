fcb0a52a8d96dbc0f02020223d7625ef7cdec4ae
Nazar Kolachko <kolachko.nazar1123@vu.cdu.edu.ua>
2023-11-22 23:00:38 +0200

    Added number calculation to the result

--- Git_Exercise/Program.cs
+++ Git_Exercise/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Git_Exercise
+{
+    class Exercise
+    {
+        static void Main(string[] args)
+        {
+            int x = 5;
+            int y = 10;
+            Console.WriteLine("5 + 10 = {0}", x + y);
+            Console.ReadKey();
+        }
+    }
+}
